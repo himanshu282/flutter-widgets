302f8da85b658bcef08bfffe2c97e7afd5fecb38
Himanshu <mrhimanshu282@gmail.com>
2023-06-30 16:51:44 +0530

    feat: core update

---
+++ packages/syncfusion_flutter_xlsio/example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/bigoh/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/bigoh/Documents/flutter-graph/packages/syncfusion_flutter_xlsio/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
